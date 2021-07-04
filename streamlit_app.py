5ddcfd424ab1f4d33585aa832fac5f71f52d97c9
Charline_T <79654866+Charline-Thomasson@users.noreply.github.com>
2021-07-04 13:18:56 +0200

    added requirements and py script
    added requirements text file and python script

---
+++ streamlit_app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+# st.title('Hello Wilders, welcome to my application!')
+
+# st.write("I enjoy to discover stremalit possibilities")
+
+### importing a DF and displaying it
+# link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
+# df_weather = pd.read_csv(link)
+# st.write(df_weather)
+# st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+
+### viz with seaborn or matplotlib else for plotly syntax is: st.plotly_chart()
+# viz_correlation = sns.heatmap(df_weather.corr(), 
+# 								center=0,
+# 								cmap = sns.color_palette("vlag", as_cmap=True)
+# 								)
+
+# st.pyplot(viz_correlation.figure)
+
+st.title('Hello Wilders, welcome to my application!')
+
+name = st.text_input("Please give me your name :")
+name_length = len(name)
+st.write("Your name has ",name_length,"characters")
+
+np.__version__
